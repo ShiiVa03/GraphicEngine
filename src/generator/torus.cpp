6517e0cf7e72b08ef3068233d1ce715e69d8f92f
Fytex <pedropauloqt@gmail.com>
2022-05-18 11:51:01 +0100

    Normals of the torus

--- src/generator/torus.cpp
+++ src/generator/torus.cpp
@@ -36,45 +36,52 @@ std::tuple<std::vector<Point>, std::vector<Vector>, std::vector<Point2D>> Torus:
         SphericalCoord sc1(i * slice_size, 0, radius_ring);
         SphericalCoord sc2((i + 1) * slice_size, 0, radius_ring);
 
-        Vector vec1(Point(0, 0, 0), Point(SphericalCoord(i * slice_size, 0, radius_center)));
-        Vector vec2(Point(0, 0, 0), Point(SphericalCoord((i + 1) * slice_size, 0, radius_center)));
+        Point radius_center_p1(SphericalCoord(i * slice_size, 0, radius_center));
+        Point radius_center_p2(SphericalCoord((i + 1) * slice_size, 0, radius_center));
+
+        Vector vec1(Point(0, 0, 0), radius_center_p1);
+        Vector vec2(Point(0, 0, 0), radius_center_p2);
 
 
         for (int j = 0; j < stacks; ++j) {
             Point p_bottom_left(sc1);
             Point p_bottom_right(sc2);
 
-            Vector normal1(p_bottom_left.x/radius_ring, p_bottom_left.y/radius_ring, p_bottom_left.z/radius_ring);
-            Vector normal2(p_bottom_right.x/radius_ring, p_bottom_right.y/radius_ring, p_bottom_right.z/radius_ring);
-
             sc1.beta += stack_size;
             sc2.beta += stack_size;
             Point p_top_left(sc1);
             Point p_top_right(sc2);
-
-            Vector normal3(p_top_left.x / radius_ring, p_top_left.y/radius_ring, p_top_right.x/radius_ring);
-            Vector normal4(p_top_right.x / radius_ring, p_top_right.y/radius_ring, p_top_right.x/radius_ring);
 
             p_bottom_left.addVector(vec1);
             p_top_left.addVector(vec1);
             p_bottom_right.addVector(vec2);
             p_top_right.addVector(vec2);
 
+            Vector normal_bottom_left(radius_center_p1, p_bottom_left);
+            Vector normal_top_left(radius_center_p1, p_top_left);
+            Vector normal_bottom_right(radius_center_p2, p_bottom_right);
+            Vector normal_top_right(radius_center_p2, p_top_right);
+
+            normal_bottom_left.normalize();
+            normal_top_left.normalize();
+            normal_bottom_right.normalize();
+            normal_top_right.normalize();
+
             points.push_back(p_bottom_left);
             points.push_back(p_bottom_right);
             points.push_back(p_top_right);
 
-            normals.push_back(normal1);
-            normals.push_back(normal2);
-            normals.push_back(normal4);
+            normals.push_back(normal_bottom_left);
+            normals.push_back(normal_bottom_right);
+            normals.push_back(normal_top_right);
 
             points.push_back(p_bottom_left);
             points.push_back(p_top_right);
             points.push_back(p_top_left);
 
-            normals.push_back(normal1);
-            normals.push_back(normal4);
-            normals.push_back(normal3);
+            normals.push_back(normal_bottom_left);
+            normals.push_back(normal_top_right);
+            normals.push_back(normal_top_left);
         }
 
     }
