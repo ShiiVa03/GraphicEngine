d390b4d83b00fdef018a56021b65b578c6d59325
Fytex <pedropauloqt@gmail.com>
2022-06-10 12:36:16 +0100

    Name Change

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -549,7 +549,7 @@ int main(int argc, char ** argv) {
     glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowPosition(100, 100);
     glutInitWindowSize(800, 800);
-    glutCreateWindow("CG@DI-UM");
+    glutCreateWindow("GraphicEngine");
 
     // init GLEW
 #ifndef __APPLE__
